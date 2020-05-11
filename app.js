e356092d80c696f346d0d125df2a6f4b9fa5f697
Raghavendra Guttur <strikingraghu@gmail.com>
2020-05-11 22:41:22 +0530

    C2D

---
+++ app.js
@@ -0,0 +1,15 @@
+//jshint esversion: 6
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+app.use(express.static("public"));
+
+
+app.listen(3000, function(req, res) {
+    console.log("Server Running")
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
